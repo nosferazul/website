e7cdbd45ecfe50442ec53636357955f09935919b
nosferazul <zulhazan5@hotmail.com>
2023-05-05 16:04:33 +0700

    add ecom

---
+++ ecom/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Customer,Product,Orders,Feedback,Checkreceipt
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'profile_pic')
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'product_image')
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Orders, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
+# Register your models here.
+
+class CheckreceiptAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'receipt_image')
+admin.site.register(Checkreceipt, CheckreceiptAdmin)
+
